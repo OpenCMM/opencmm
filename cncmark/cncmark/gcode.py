3ad734f93bf6227ef6e3fee6495f866211d31396
yuichiroaoki <45054071+yuichiroaoki@users.noreply.github.com>
2023-08-12 09:35:40 +0900

    update gcode

--- cncmark/cncmark/gcode.py
+++ cncmark/cncmark/gcode.py
@@ -1,11 +1,28 @@
-def get_gcode(points):
-    gcode = []
-    for point in points:
-        gcode.append(f"G1 X{point[0]} Y{point[1]} Z{point[2]}")
-    return gcode
+import numpy as np
+class GCode:
+    def __init__(self, points, feed_rate=1000):
+        self.points = points
+        self.feed_rate = feed_rate
+        self.gcode = []
+        self.pic_wait = []
 
+    def generate_gcode(self):
+        start = np.array([0, 0, 0])
+        f_per_sec = self.feed_rate / 60
+        for point in self.points:
+            # wait 1 second
+            self.gcode.append("G4 P1")
 
-def save_gcode(gcode, file_path: str):
-    with open(file_path, "w") as f:
-        for line in gcode:
-            f.write(line + "\n")
+            time_to_move = self.distance(start, point) / f_per_sec
+            self.pic_wait.append(1 + time_to_move)
+            one_line = f"G1 X{point[0]} Y{point[1]} Z{point[2]} F{self.feed_rate}"
+            self.gcode.append(one_line)
+            start = point
+
+    def save_gcode(self, file_path: str):
+        with open(file_path, "w") as f:
+            for line in self.gcode:
+                f.write(line + "\n")
+
+    def distance(self, a, b):
+        return np.linalg.norm(a - b)
