0caf72c82dacf3015a8cebab4bbd77de2bf79e4f
yuichiroaoki <45054071+yuichiroaoki@users.noreply.github.com>
2023-08-12 09:43:28 +0900

    camera height

--- cncmark/cncmark/gcode.py
+++ cncmark/cncmark/gcode.py
@@ -1,8 +1,17 @@
 import numpy as np
+
+
 class GCode:
-    def __init__(self, points, feed_rate=1000):
+    def __init__(self, points, feed_rate, camera_height):
+        """
+        :param points: numpy array of points
+        :param feed_rate: feed rate in mm/min
+        :param camera_height: height of camera in mm
+        :return: GCode object
+        """
         self.points = points
         self.feed_rate = feed_rate
+        self.camera_height = camera_height
         self.gcode = []
         self.pic_wait = []
 
@@ -15,7 +24,8 @@ class GCode:
 
             time_to_move = self.distance(start, point) / f_per_sec
             self.pic_wait.append(1 + time_to_move)
-            one_line = f"G1 X{point[0]} Y{point[1]} Z{point[2]} F{self.feed_rate}"
+            z = point[2] + self.camera_height
+            one_line = f"G1 X{point[0]} Y{point[1]} Z{z} F{self.feed_rate}"
             self.gcode.append(one_line)
             start = point
 
--- cncmark/tests/test_gcode.py
+++ cncmark/tests/test_gcode.py
@@ -5,15 +5,15 @@ import numpy as np
 
 def test_gcode():
     points = np.array([[0, 10, 0], [0, 20, 0], [0, 30, 0]])
-    gcode = GCode(points, 600)
+    gcode = GCode(points, 600, 300)
     gcode.generate_gcode()
     assert len(gcode.gcode) == 6
     assert gcode.gcode[0] == "G4 P1"
-    assert gcode.gcode[1] == "G1 X0 Y10 Z0 F600"
+    assert gcode.gcode[1] == "G1 X0 Y10 Z300 F600"
     assert gcode.gcode[2] == "G4 P1"
-    assert gcode.gcode[3] == "G1 X0 Y20 Z0 F600"
+    assert gcode.gcode[3] == "G1 X0 Y20 Z300 F600"
     assert gcode.gcode[4] == "G4 P1"
-    assert gcode.gcode[5] == "G1 X0 Y30 Z0 F600"
+    assert gcode.gcode[5] == "G1 X0 Y30 Z300 F600"
 
     for wait in gcode.pic_wait:
         assert wait == 2.0
@@ -21,14 +21,14 @@ def test_gcode():
     z = 10.0
     lines = get_lines("tests/fixtures/stl/sample.stl", z)
     unique_points = get_unique_points(lines)
-    gcode = GCode(unique_points)
+    gcode = GCode(unique_points, 600, 300)
     gcode.generate_gcode()
     assert len(gcode.gcode) == 24
 
 
 def test_pic_wait():
     points = np.array([[0, 10, 0], [30, 10, 0], [30, 30, 0]])
-    gcode = GCode(points, 600)
+    gcode = GCode(points, 600, 300)
     gcode.generate_gcode()
     assert len(gcode.gcode) == 6
     assert gcode.pic_wait[0] == 2.0
