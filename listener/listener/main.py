890ba5bb3241d575f796bc3412c8182dd2771e8c
yuichiroaoki <45054071+yuichiroaoki@users.noreply.github.com>
2023-10-17 15:49:15 +0900

    remove thread

--- listener/listener/main.py
+++ listener/listener/main.py
@@ -19,20 +19,21 @@ chunk_size = 1024
 # position_path = './/{urn:mtconnect.org:MTConnectStreams:2.0}Position'
 position_path = ".//{urn:mtconnect.org:MTConnectStreams:1.3}Position"
 xyz = None
-distance = None
 initial_coordinate = (109.074, -15.028, -561.215)
 final_coordinate = (105.042, -11.028, -568.215)
-streaming = False
 done = False
+data_to_insert = []
 
 
 async def receive_sensor_data():
-    global distance
+    global data_to_insert
     async with websockets.connect(server_url) as websocket:
         print("receive_sensor_data(): connected")
 
         while not done:
             distance = await websocket.recv()
+            (x, y, z) = xyz
+            data_to_insert.append((x, y, z, float(distance)))
 
 
 # ref. https://stackoverflow.com/questions/67734115/how-to-use-multithreading-with-websockets
@@ -51,46 +52,45 @@ def contorl_streaming_status():
 
 
 async def control_sensor():
-    global streaming, xyz, done
+    streaming = False
+    global done
     async with websockets.connect(server_url) as websocket:
         print("control_sensor(): connected")
 
+        idx = 0
         while not done:
-            if not streaming and xyz is not None and initial_coordinate == xyz:
+            idx += 1
+            if not streaming and xyz is not None:
+                # if not streaming and xyz is not None and initial_coordinate == xyz:
                 print("ready to start streaming")
                 await websocket.send("startStreaming")
                 print("start streaming")
                 streaming = True
 
-            elif streaming and xyz is not None and final_coordinate == xyz:
+            elif streaming and xyz is not None and idx == 40:
+                # elif streaming and xyz is not None and final_coordinate == xyz:
                 print("ready to stop streaming")
                 await websocket.send("stopStreaming")
                 # await websocket.send("deepSleep")
                 print("stop streaming")
                 streaming = False
                 done = True
+                combine_data()
 
             await asyncio.sleep(0.5)
 
 
 def combine_data():
-    global xyz, distance
-    mysql_conn = mysql.connector.connect(**MYSQL_CONFIG, database="coord")
+    # Perform a bulk insert
+    mysql_config = dict(
+        host="192.168.122.76",
+        port=3306,
+        user="root",
+        password="root",
+    )
+    mysql_conn = mysql.connector.connect(**mysql_config, database="coord")
     mysql_cur = mysql_conn.cursor()
 
-    data_to_insert = []
-    while not done:
-        if distance is not None and xyz is not None:
-            # Combine sensor_data and coordinate_data
-            combined_data = (*xyz, float(distance))
-            (x, y, z) = xyz
-            print(combined_data)
-            data_to_insert.append((x, y, z, float(distance)))
-            # Reset the variables to wait for new data
-            distance = None
-            xyz = None
-
-    # Perform a bulk insert
     mysql_cur.executemany(
         "INSERT INTO sensor(x, y, z, distance) VALUES (%s, %s, %s, %s)", data_to_insert
     )
@@ -102,7 +102,7 @@ def combine_data():
 def mtconnect_streaming_reader():
     global xyz
     # demo
-    # endpoint = "https://demo.metalogi.io/sample?path=//Axes/Components/Linear/DataItems/DataItem&interval=1000"
+    # endpoint = "https://demo.metalogi.io/current?path=//Axes/Components/Linear/DataItems/DataItem&interval=1000"
 
     endpoint = "http://192.168.0.19:5000/current?path=//Axes/Components/Linear/DataItems&interval=1000"
     try:
@@ -135,7 +135,6 @@ def mtconnect_streaming_reader():
                     # full xml data received
                     try:
                         xyz = mt.get_coordinates(xml_buffer, position_path)[:3]
-
                     except ET.ParseError:
                         print("ParseError")
 
@@ -151,17 +150,14 @@ def mtconnect_streaming_reader():
 def listener_start():
     thread1 = threading.Thread(target=listen_sensor)
     thread2 = threading.Thread(target=mtconnect_streaming_reader)
-    thread3 = threading.Thread(target=combine_data)
-    thread4 = threading.Thread(target=contorl_streaming_status)
+    thread3 = threading.Thread(target=contorl_streaming_status)
 
     # Start the threads
     thread1.start()
     thread2.start()
     thread3.start()
-    thread4.start()
 
     # Join the threads (optional, to wait for them to finish)
     thread1.join()
     thread2.join()
     thread3.join()
-    thread4.join()
