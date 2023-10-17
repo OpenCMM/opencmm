155d65eafbc2f1d0458b8dabfc0d6c91ec5a9f56
yuichiroaoki <45054071+yuichiroaoki@users.noreply.github.com>
2023-10-17 20:31:01 +0900

    start measurement

--- ui/src/routes/start/+page.svelte
+++ ui/src/routes/start/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import DownloadGCode from './DownloadGCode.svelte';
+	import StartSensor from './StartSensor.svelte';
 </script>
 
 <DownloadGCode />
+<StartSensor />
---
+++ ui/src/routes/start/StartSensor.svelte
@@ -0,0 +1,55 @@
+<!-- Start capturing images -->
+
+<script lang="ts">
+	import { BACKEND_URL_LOCAL } from '$lib/constants/backend';
+	import axios from 'axios';
+	import { Form, FormGroup, TextInput, Button, InlineLoading } from 'carbon-components-svelte';
+	import { _ } from 'svelte-i18n';
+
+	let error: string | null = null;
+	let mtInterval = 500;
+	let processing = false;
+
+	const startCapturing = async (e: Event) => {
+		e.preventDefault();
+
+		try {
+			processing = true;
+			const res = await axios.post(`${BACKEND_URL_LOCAL}/start/measurement/${mtInterval}`);
+			console.log(res);
+		} catch (err) {
+			console.error(err);
+			error = $_('home.start.error');
+			processing = false;
+		}
+	};
+</script>
+
+<div class="bx--form-item">
+	<p id="form-title">{$_('home.start.title')}</p>
+
+	<Form on:submit={startCapturing}>
+		<FormGroup>
+			<TextInput labelText="MTConnect data fetch interval" id="mtInterval" bind:value={mtInterval} />
+		</FormGroup>
+		{#if processing && !error}
+			<InlineLoading status="active" description={$_('home.start.status.active')} />
+		{:else if error}
+			<InlineLoading status="error" description={error} />
+		{:else}
+			<Button type="submit">{$_('home.start.start')}</Button>
+		{/if}
+	</Form>
+</div>
+
+<style>
+	.bx--form-item {
+		margin: 1rem;
+	}
+
+	#form-title {
+		font-size: 1.2rem;
+		font-weight: bold;
+		margin: 1rem;
+	}
+</style>
