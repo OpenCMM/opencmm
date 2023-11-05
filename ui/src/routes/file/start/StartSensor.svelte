08185cc5b077e875d232778bb24a638c18ebac13
yuichiroaoki <45054071+yuichiroaoki@users.noreply.github.com>
2023-11-05 23:56:39 +0900

    delete start measurement button

--- ui/src/routes/file/start/StartSensor.svelte
+++ ui/src/routes/file/start/StartSensor.svelte
@@ -1,7 +1,4 @@
 <script lang="ts">
-	import { BACKEND_URL } from '$lib/constants/backend';
-	import axios from 'axios';
-	import { Form, FormGroup, TextInput, Button } from 'carbon-components-svelte';
 	import { _ } from 'svelte-i18n';
 	import { page } from '$app/stores';
 	import DownloadGCode from './DownloadGCode.svelte';
@@ -9,31 +6,8 @@
 	import SensorStatus from './SensorStatus.svelte';
 
 	const modelId = $page.url.searchParams.get('id');
-	const advanced = $page.url.searchParams.get('advanced') === 'true';
-
 	let error: string | null = null;
-	let mtInterval = 5;
-	let interval = 10;
-	let threshold = 100;
 	let sensorStatus = 'process not found';
-
-	const startCapturing = async (e: Event) => {
-		e.preventDefault();
-
-		try {
-			const data = {
-				three_d_model_id: Number(modelId),
-				mtconnect_interval: Number(mtInterval),
-				interval: Number(interval),
-				threshold: Number(threshold)
-			};
-			const res = await axios.post(`${BACKEND_URL}/start/measurement`, data);
-			console.log(res);
-		} catch (err) {
-			console.error(err);
-			error = $_('home.start.error');
-		}
-	};
 </script>
 
 <div class="steps-to-measure">
@@ -41,39 +15,10 @@
 	<p>{$_('home.start.howto.step1')}</p>
 	<p>{$_('home.start.howto.step2')}</p>
 	<p>{$_('home.start.howto.step3')}</p>
-	<p>{$_('home.start.howto.step4')}</p>
 </div>
 
 {#if modelId}
 	<DownloadGCode {modelId} />
-{/if}
-
-{#if sensorStatus === 'process not found'}
-	<div class="bx--form-item">
-		<h3>{$_('home.start.title')}</h3>
-		<p class="start-description">{$_('home.start.description')}</p>
-
-		<Form on:submit={startCapturing}>
-			{#if advanced}
-				<FormGroup>
-					<TextInput
-						labelText="MTConnect data fetch interval"
-						id="mtInterval"
-						bind:value={mtInterval}
-					/>
-				</FormGroup>
-				<FormGroup>
-					<TextInput labelText="Sensor data fetch interval" id="interval" bind:value={interval} />
-					<TextInput
-						labelText="Sensor data difference threshold"
-						id="threshold"
-						bind:value={threshold}
-					/>
-				</FormGroup>
-			{/if}
-			<Button type="submit">{$_('home.start.start')}</Button>
-		</Form>
-	</div>
 {/if}
 
 {#if modelId}
@@ -85,19 +30,9 @@
 {/if}
 
 <style>
-	.bx--form-item {
-		margin-top: 2rem;
-	}
-
 	h2 {
 		margin-bottom: 1rem;
 	}
-
-	.start-description {
-		margin-top: 1rem;
-		margin-bottom: 1rem;
-	}
-
 	.steps-to-measure {
 		margin-top: 2rem;
 		margin-bottom: 2rem;
