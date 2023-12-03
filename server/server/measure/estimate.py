56e6efc49b968900dbcb0abf3d6f583d5f93c9eb
yuichiroaoki <45054071+yuichiroaoki@users.noreply.github.com>
2023-12-04 00:37:05 +0900

    refactor estimate

--- server/server/measure/estimate.py
+++ server/server/measure/estimate.py
@@ -44,6 +44,19 @@ class Result:
         self.process_id = process_id
         self.conf = get_config()
 
+        mtconnect_data = get_mtconnect_data(process_id, mysql_config)
+        self.np_mtconnect_data = np.array(mtconnect_data)
+        sensor_data = get_sensor_data(process_id, mysql_config)
+        self.np_sensor_data = np.array(sensor_data)
+
+        model_row = get_model_data(model_id)
+        filename = model_row[1]
+        # offset = (model_row[3], model_row[4], model_row[5])
+        gcode_filename = get_gcode_filename(filename)
+        gcode_file_path = f"{GCODE_PATH}/{gcode_filename}"
+        self.gcode = load_gcode(gcode_file_path)
+        self.z = self.np_mtconnect_data[0][5]
+
     def sensor_timestamp_to_coord(
         self,
         start_timestamp: datetime,
@@ -71,19 +84,41 @@ class Result:
         coord = np.round(coord, 3)
         return tuple(coord)
 
-    def compute_edge_results(self):
+    def get_edge_results(self, mtconnect_row, line):
         mtconnect_latency = self.conf["mtconnect"]["latency"]
-        model_row = get_model_data(self.model_id)
-        filename = model_row[1]
-        # offset = (model_row[3], model_row[4], model_row[5])
-        gcode_filename = get_gcode_filename(filename)
-        gcode_file_path = f"{GCODE_PATH}/{gcode_filename}"
-        gcode = load_gcode(gcode_file_path)
-        mtconnect_data = get_mtconnect_data(self.process_id, self.mysql_config)
-        np_mtconnect_data = np.array(mtconnect_data)
-        z = np_mtconnect_data[0][5]
-        sensor_data = get_sensor_data(self.process_id, self.mysql_config)
-        np_sensor_data = np.array(sensor_data)
+        xy = (mtconnect_row[3], mtconnect_row[4])
+        _timestamp = mtconnect_row[2] - timedelta(milliseconds=mtconnect_latency)
+        # feedrate = row[7] # feedrate from MTConnect is not accurate
+        (start, end, feedrate) = get_start_end_points_from_line_number(self.gcode, line)
+        # get timestamp at start and end
+        start_timestamp = get_timestamp_at_point(xy, start, feedrate, _timestamp, True)
+        end_timestamp = get_timestamp_at_point(xy, end, feedrate, _timestamp, False)
+
+        # get sensor data that is between start and end timestamp
+        for sensor_row in self.np_sensor_data:
+            sensor_timestamp = sensor_row[2]
+            if start_timestamp <= sensor_timestamp <= end_timestamp:
+                direction_vector = np.array([end[0] - start[0], end[1] - start[1]])
+                distance = np.linalg.norm(direction_vector)
+                direction_vector = tuple(direction_vector / distance)
+                edge_coord = self.sensor_timestamp_to_coord(
+                    start_timestamp,
+                    sensor_timestamp,
+                    start,
+                    direction_vector,
+                    feedrate,
+                )
+                edge_id = get_edge_id_from_line_number(
+                    self.mysql_config, self.model_id, line
+                )
+                # ignore the rest of the sensor data
+                # multiple edges can be measured due to the following reasons:
+                # - noise (can be reduced by increasing the sensor threshold)
+                # - sensor restart
+                # - timestamp is not accurate
+                return (edge_id, self.process_id, edge_coord[0], edge_coord[1], self.z)
+
+    def compute_edge_results(self):
         first_line_for_tracing = get_first_line_number_for_tracing(
             self.mysql_config, self.model_id
         )
@@ -91,10 +126,10 @@ class Result:
         current_line = 0
         edge_update_list = []
         step_update_list = []
-        for row in np_mtconnect_data:
+        for row in self.np_mtconnect_data:
             xy = (row[3], row[4])
             line = int(row[6])
-            line = get_true_line_number(xy, line, gcode, first_line_for_tracing)
+            line = get_true_line_number(xy, line, self.gcode, first_line_for_tracing)
             if not line:
                 # Not on line
                 continue
@@ -108,47 +143,9 @@ class Result:
                     # Not measuring
                     continue
 
-                _timestamp = row[2] - timedelta(milliseconds=mtconnect_latency)
-                # feedrate = row[7] # feedrate from MTConnect is not accurate
-                (start, end, feedrate) = get_start_end_points_from_line_number(
-                    gcode, line
-                )
-                # get timestamp at start and end
-                start_timestamp = get_timestamp_at_point(
-                    xy, start, feedrate, _timestamp, True
-                )
-                end_timestamp = get_timestamp_at_point(
-                    xy, end, feedrate, _timestamp, False
-                )
-
-                # get sensor data that is between start and end timestamp
-                for sensor_row in np_sensor_data:
-                    sensor_timestamp = sensor_row[2]
-                    if start_timestamp <= sensor_timestamp <= end_timestamp:
-                        direction_vector = np.array(
-                            [end[0] - start[0], end[1] - start[1]]
-                        )
-                        distance = np.linalg.norm(direction_vector)
-                        direction_vector = tuple(direction_vector / distance)
-                        edge_coord = self.sensor_timestamp_to_coord(
-                            start_timestamp,
-                            sensor_timestamp,
-                            start,
-                            direction_vector,
-                            feedrate,
-                        )
-                        edge_id = get_edge_id_from_line_number(
-                            self.mysql_config, self.model_id, line
-                        )
-                        edge_update_list.append(
-                            (edge_id, self.process_id, edge_coord[0], edge_coord[1], z)
-                        )
-                        # ignore the rest of the sensor data
-                        # multiple edges can be measured due to the following reasons:
-                        # - noise (can be reduced by increasing the sensor threshold)
-                        # - sensor restart
-                        # - timestamp is not accurate
-                        break
+                edge_result = self.get_edge_results(row, line)
+                if edge_result:
+                    edge_update_list.append(edge_result)
 
             # tracing
             else:
@@ -156,9 +153,10 @@ class Result:
                     # Not measuring
                     continue
 
+                mtconnect_latency = self.conf["mtconnect"]["latency"]
                 _timestamp = row[2] - timedelta(milliseconds=mtconnect_latency)
                 (start, end, feedrate) = get_start_end_points_from_line_number(
-                    gcode, line
+                    self.gcode, line
                 )
                 # get timestamp at start and end
                 start_timestamp = get_timestamp_at_point(
@@ -171,7 +169,7 @@ class Result:
                 count = 0
                 total_sensor_output = 0
                 # get sensor data that is between start and end timestamp
-                for sensor_row in np_sensor_data:
+                for sensor_row in self.np_sensor_data:
                     sensor_timestamp = sensor_row[2]
                     if start_timestamp <= sensor_timestamp <= end_timestamp:
                         total_sensor_output += sensor_row[3]
@@ -218,9 +216,7 @@ def update_data_after_measurement(
     client.connect(MQTT_BROKER_URL, 1883, 60)
 
     result = Result(mysql_config, model_id, process_id)
-    edge_update_list, step_update_list = result.compute_edge_results(
-        mysql_config, model_id, process_id
-    )
+    edge_update_list, step_update_list = result.compute_edge_results()
     edge_count = len(edge_update_list)
     if edge_count == 0:
         status.update_process_status(
@@ -262,9 +258,7 @@ def recompute(mysql_config: dict, process_id: int):
     process_data = status.get_process_status(mysql_config, process_id)
     model_id = process_data[1]
     result = Result(mysql_config, model_id, process_id)
-    edge_update_list, step_update_list = result.compute_edge_results(
-        mysql_config, model_id, process_id
-    )
+    edge_update_list, step_update_list = result.compute_edge_results()
 
     edge_count = len(edge_update_list)
     if edge_count == 0:
