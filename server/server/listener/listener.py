5996632c7eefff4c4a31faee103688f64395f570
yuichiroaoki <45054071+yuichiroaoki@users.noreply.github.com>
2023-12-13 15:35:23 +0900

    use mqtt

--- server/server/listener/listener.py
+++ server/server/listener/listener.py
@@ -4,6 +4,7 @@ from server.config import (
     get_config,
 )
 from . import hakaru, mt
+from .mtmqtt import listen_data_with_mqtt
 import logging
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s:%(message)s")
@@ -18,6 +19,7 @@ def listener_start(
     mtconnect_interval = conf["mtconnect"]["interval"]
     sensor_interval = conf["sensor"]["interval"]
     sensor_threshold = conf["sensor"]["threshold"]
+    disable_mtct = conf["mtconnect"]["disable"]
 
     thread1 = threading.Thread(
         target=hakaru.listen_sensor,
@@ -30,28 +32,50 @@ def listener_start(
             )
         ),
     )
-    thread2 = threading.Thread(
-        target=mt.mtconnect_streaming_reader,
-        args=(
-            (
-                mtconnect_interval,
-                mysql_config,
-                process_id,
-                MQTT_BROKER_URL,
-            )
-        ),
-    )
-    thread3 = threading.Thread(
-        target=mt.stop_mtconnect_reader,
-        args=((MQTT_BROKER_URL,)),
-    )
 
-    # Start the threads
-    thread1.start()
-    thread2.start()
-    thread3.start()
+    if disable_mtct:
+        thread2 = threading.Thread(
+            target=listen_data_with_mqtt,
+            args=(
+                (
+                    mysql_config,
+                    process_id,
+                    MQTT_BROKER_URL,
+                )
+            ),
+        )
 
-    # Join the threads (optional, to wait for them to finish)
-    thread1.join()
-    thread2.join()
-    thread3.join()
+        # Start the threads
+        thread1.start()
+        thread2.start()
+
+        # Join the threads (optional, to wait for them to finish)
+        thread1.join()
+        thread2.join()
+
+    else:
+        thread2 = threading.Thread(
+            target=mt.mtconnect_streaming_reader,
+            args=(
+                (
+                    mtconnect_interval,
+                    mysql_config,
+                    process_id,
+                    MQTT_BROKER_URL,
+                )
+            ),
+        )
+        thread3 = threading.Thread(
+            target=mt.stop_mtconnect_reader,
+            args=((MQTT_BROKER_URL,)),
+        )
+
+        # Start the threads
+        thread1.start()
+        thread2.start()
+        thread3.start()
+
+        # Join the threads (optional, to wait for them to finish)
+        thread1.join()
+        thread2.join()
+        thread3.join()
