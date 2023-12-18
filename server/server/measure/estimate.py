b89d118236718e36214c1d81fad03f3d8c622cf1
yuichiroaoki <45054071+yuichiroaoki@users.noreply.github.com>
2023-12-18 20:41:35 +0900

    refactor compute_updating_data()

--- server/server/measure/estimate.py
+++ server/server/measure/estimate.py
@@ -7,7 +7,6 @@ from server.config import (
     MQTT_BROKER_URL,
     MQTT_PASSWORD,
     MQTT_USERNAME,
-    get_config,
 )
 from time import time
 from server.mark.edge import (
@@ -31,7 +30,6 @@ from server.mark.trace import (
 from server.mark.trace import import_trace_line_results
 import logging
 from scipy.ndimage import uniform_filter1d
-import trimesh
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s: %(message)s")
 logger = logging.getLogger(__name__)
@@ -42,7 +40,6 @@ class Result:
         self.mysql_config = mysql_config
         self.model_id = model_id
         self.process_id = process_id
-        self.conf = get_config()
 
         self.mtct_data_checker = MtctDataChecker(mysql_config, model_id, process_id)
         self.mtct_lines = self.mtct_data_checker.estimate_timestamps_from_mtct_data()
@@ -53,7 +50,6 @@ class Result:
         model_row = get_model_data(model_id)
         filename = model_row[1]
         self.stl_filepath = f"{MODEL_PATH}/{filename}"
-        self.mesh = trimesh.load(self.stl_filepath)
         process_status = status.get_process_status(mysql_config, process_id)
         self.offset = (process_status[4], process_status[5], process_status[6])
         gcode_filename = get_gcode_filename(filename)
@@ -70,60 +66,67 @@ class Result:
         (start, end, feedrate) = get_start_end_points_from_line_number(self.gcode, line)
 
         # get sensor data that is between start and end timestamp
-        for sensor_row in self.np_sensor_data:
+        sensor_data_during_line = self.np_sensor_data[
+            np.logical_and(
+                self.np_sensor_data[:, 2] >= start_timestamp,
+                self.np_sensor_data[:, 2] <= end_timestamp,
+            )
+        ]
+        for sensor_row in sensor_data_during_line:
             sensor_timestamp = sensor_row[2]
             sensor_output = sensor_row[3]
-            if start_timestamp <= sensor_timestamp <= end_timestamp:
-                (
-                    sensor_output_valid,
-                    edge_ids,
-                ) = self.mtct_data_checker.validate_sensor_output(sensor_output, line)
-                if not sensor_output_valid:
-                    continue
-                edge_coord = self.mtct_data_checker.sensor_timestamp_to_coord(
-                    start_timestamp,
-                    sensor_timestamp,
-                    start,
-                    end,
-                    feedrate,
-                )
-                """
-                when edges are overlapping, multiple edges can be measured
-                for a single line
-                """
-                # edge_ids = get_edge_id_from_line_number(
-                #     self.mysql_config, self.model_id, line
-                # )
+            (
+                sensor_output_valid,
+                edge_ids,
+            ) = self.mtct_data_checker.validate_sensor_output(sensor_output, line)
+            if not sensor_output_valid:
+                continue
+            edge_coord = self.mtct_data_checker.sensor_timestamp_to_coord(
+                start_timestamp,
+                sensor_timestamp,
+                start,
+                end,
+                feedrate,
+            )
+            """
+            when edges are overlapping, multiple edges can be measured
+            for a single line
+            """
+            # edge_ids = get_edge_id_from_line_number(
+            #     self.mysql_config, self.model_id, line
+            # )
 
-                # ignore the rest of the sensor data
-                # multiple edges can be measured due to the following reasons:
-                # - noise (can be reduced by increasing the sensor threshold)
-                # - sensor restart
-                # - timestamp is not accurate
-                results = []
-                measured_z = round(sensor_output_to_mm(sensor_output), 3)
-                for edge_id in edge_ids:
-                    results.append(
-                        (
-                            edge_id,
-                            self.process_id,
-                            edge_coord[0],
-                            edge_coord[1],
-                            measured_z,
-                        )
+            # ignore the rest of the sensor data
+            # multiple edges can be measured due to the following reasons:
+            # - noise (can be reduced by increasing the sensor threshold)
+            # - sensor restart
+            # - timestamp is not accurate
+            results = []
+            measured_z = round(sensor_output_to_mm(sensor_output), 3)
+            for edge_id in edge_ids:
+                results.append(
+                    (
+                        edge_id,
+                        self.process_id,
+                        edge_coord[0],
+                        edge_coord[1],
+                        measured_z,
                     )
-                return results
+                )
+            return results
 
     def get_trace_line_result(self, start_timestamp, end_timestamp, line):
         """
         Estimate the approximate distance between the workpiece and the sensor
         """
-        distance_results = []
         # get sensor data that is between start and end timestamp
-        for sensor_row in self.np_sensor_data:
-            sensor_timestamp = sensor_row[2]
-            if start_timestamp <= sensor_timestamp <= end_timestamp:
-                distance_results.append(sensor_row[3])
+        sensor_data_during_line = self.np_sensor_data[
+            np.logical_and(
+                self.np_sensor_data[:, 2] >= start_timestamp,
+                self.np_sensor_data[:, 2] <= end_timestamp,
+            )
+        ]
+        distance_results = sensor_data_during_line[:, 3]
 
         # Apply moving average filter to remove fluctuations
         filtered_distance_results = uniform_filter1d(distance_results, size=5)
@@ -258,7 +261,9 @@ def recompute(mysql_config: dict, process_id: int):
     process_data = status.get_process_status(mysql_config, process_id)
     model_id = process_data[1]
     result = Result(mysql_config, model_id, process_id)
+    compute_start_time = time()
     edge_update_list, trace_line_update_list = result.compute_updating_data()
+    logger.info(f"compute_updating_data() took {time() - compute_start_time} seconds")
     logger.info("compute_updating_data() done")
 
     edge_count = len(edge_update_list)
